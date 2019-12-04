e0380dae637d946d0165d5250da63ce43bb4b123
Raul Escobar <raul13escobar@gmail.com>
2019-12-04 09:10:33 +0100

    compress images for faster load (#171)

--- src/components/ResizedImage/ResizedImage.js
+++ src/components/ResizedImage/ResizedImage.js
@@ -4,7 +4,7 @@ import LazyLoader from 'components/LazyLoader'
 const getImageUrlWithParams = (url, width, height, fit) =>
   `${process.env.REACT_APP_IMGIX_URL}/${encodeURI(
     url
-  )}?w=${width}&h=${height}&fit=${fit}&crop=edges&auto=format&auto=enhance&vib=30`
+  )}?w=${width}&h=${height}&fit=${fit}&crop=edges&auto=format,enhance,compress&vib=30`
 
 const ResizedImage = ({ src: _src, alt, width, height, fit = 'crop', ...props }) => {
   const src = getImageUrlWithParams(_src, width, height, fit)
