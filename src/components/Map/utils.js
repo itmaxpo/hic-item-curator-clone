a3011448f8aca16ce45ccb6875a88f21534a5278
RaulEscobarRivas <raul.escobar@tourlane.com>
2020-04-29 11:08:06 +0200

    fix address options

--- src/components/Map/utils.js
+++ src/components/Map/utils.js
@@ -1,8 +1,8 @@
-import { get } from 'lodash'
+import { get, omit } from 'lodash'
 
 export const parseSearchBoxResponse = data =>
   data.map(address => ({
     label: get(address, 'display_name'),
     value: get(address, 'display_name'),
-    ...address
+    ...omit(address, 'icon')
   }))
