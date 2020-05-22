9c1e015586cca9aac5a97b34d61b5d5f2d3fa796
SherryH <enjoy.wind7rain@gmail.com>
2020-05-22 16:59:10 +0800

    Fixed the bug

--- src/pages/Item/useFieldsRef.js
+++ src/pages/Item/useFieldsRef.js
@@ -3,20 +3,33 @@ import { useRef, useCallback } from 'react'
 export const useFieldsRef = () => {
   const rankingRef = useRef(undefined)
   const blacklistedRef = useRef(undefined)
+  const categoryRef = useRef(undefined)
 
   // create a function to update ref
   const updateFieldRef = useCallback(item => {
     rankingRef.current = item.rankingRef
     blacklistedRef.current = item.blacklistedRef
+    categoryRef.current = item.blacklistedRef
   }, [])
 
   const cleanFields = useCallback((fields, item) => {
-    let newFields
+    console.log('fields', fields)
+    let newFields = fields
+    // if the ref is falsy, that means Elephant doesn't have this field
+    // Don't save a falsy field to an non existant field in Elephant
+    // else it causes error
     if (!item.ranking && !rankingRef.current) {
       newFields = fields.filter(field => field.field_name !== 'ranking')
     }
     if (!item.blacklisted && !blacklistedRef.current) {
-      newFields = newFields.filter(field => field.field_name !== 'blacklisted')
+      newFields = newFields.filter(field => {
+        return field.field_name !== 'blacklisted'
+      })
+    }
+    if (!item.accommodation_category && !categoryRef.current) {
+      newFields = newFields.filter(field => {
+        return field.field_name !== 'accommodation_category'
+      })
     }
     return newFields
   }, [])
