d6c5e8036bf4ce0c8de40bf4af1a9a741c6c5b5b
SherryH <enjoy.wind7rain@gmail.com>
2020-05-22 17:45:42 +0800

    Fixed issue where fieldRef is not saved correct value

--- src/pages/Item/useFieldsRef.js
+++ src/pages/Item/useFieldsRef.js
@@ -1,21 +1,21 @@
 import { useRef, useCallback } from 'react'
 
-export const useFieldsRef = () => {
-  const rankingRef = useRef(undefined)
-  const blacklistedRef = useRef(undefined)
-  const categoryRef = useRef(undefined)
+export const useFieldsRef = item => {
+  const rankingRef = useRef(item.ranking)
+  const blacklistedRef = useRef(item.blacklisted)
+  const categoryRef = useRef(item.accommodation_category)
 
   // create a function to update ref
   const updateFieldRef = useCallback(item => {
-    rankingRef.current = item.rankingRef
-    blacklistedRef.current = item.blacklistedRef
-    categoryRef.current = item.blacklistedRef
+    rankingRef.current = item.ranking
+    blacklistedRef.current = item.blacklisted
+    categoryRef.current = item.accommodation_category
   }, [])
 
   const cleanFields = useCallback((fields, item) => {
     let newFields = fields
     // if the ref is falsy, that means Elephant doesn't have this field
-    // Don't save a falsy field to an non existant field in Elephant
+    // Don't save a null value to an non existant field in Elephant
     // else it causes error
     if (!item.ranking && !rankingRef.current) {
       newFields = fields.filter(field => field.field_name !== 'ranking')
