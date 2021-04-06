cc67a2485686b28271a263229b30663a3bb28303
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2021-04-06 17:24:53 +0200

    fix(auth): go to index after login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import { useHistory } from 'react-router-dom'
+
 import { BackgroundSingleCard } from 'components/Background'
 import { ReactComponent as LogoSvg } from 'icons/itemCuratorLogo.svg'
-import { authManager } from '../../utils/AuthManager'
+import { authManager } from 'utils/AuthManager'
 import { StyledP, StyledButton } from './styles'
 
-const Login = () => (
-  <BackgroundSingleCard>
-    <LogoSvg>Item Curator</LogoSvg>
-    <StyledP>Seamless and smooth item management.</StyledP>
-    <StyledButton onClick={() => authManager.loginWithPopup()}>Login with Google</StyledButton>
-  </BackgroundSingleCard>
-)
+const Login = () => {
+  let history = useHistory()
+
+  return (
+    <BackgroundSingleCard>
+      <LogoSvg>Item Curator</LogoSvg>
+      <StyledP>Seamless and smooth item management.</StyledP>
+      <StyledButton
+        onClick={async () => {
+          await authManager.loginWithPopup()
+          history.push('/')
+        }}
+      >
+        Login with Google
+      </StyledButton>
+    </BackgroundSingleCard>
+  )
+}
 
 export default Login
