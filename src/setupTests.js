a8b0e3ee0604f72aa7581ee3c13ecb5a12f46974
alireza firouzyar <alireza.firouzyar@gmail.com>
2022-12-01 14:40:27 +0100

    Trip 6736 replace enzyme with react testing library (#303)
    * test:add react testing library and and refactor AppContainers.test.js
    
    * test:refactor AppContainer test
    
    * conflict: resolve conflict
    
    * test:remove unnecessary tests
    
    * test:remove unnecessary tests
    
    * test:remove loader test
    
    * test:remove form.test
    
    Co-authored-by: Alireza Firouzyar <alireza.firoouzyar@tourlane.com>

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,44 @@
-import { configure } from 'enzyme'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import 'jest-styled-components'
+import '@testing-library/jest-dom'
 
-configure({ adapter: new Adapter() })
+// Increase timeout from 5 to 30 seconds to handle some slow tests.
+jest.setTimeout(30_000)
+
+let warn = console.warn
+console.warn = (...args) => {
+  // ignore "React.createFactory() is deprecated and will be removed in a future major release. Consider using JSX or use React.createElement() directly instead."
+  if (typeof args[0] !== 'string' || !args[0].includes('React.createFactory()')) {
+    warn(...args)
+  }
+}
+
+let error = console.error
+console.error = (...args) => {
+  // ignore React warnings
+  if (
+    typeof args[0] !== 'string' ||
+    !/(Warning: Received `%s`)|(React does not recognize)|(Invalid attribute name)/.test(args[0])
+  ) {
+    error(...args)
+  }
+}
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
+
+Object.defineProperty(window, 'confirm', {
+  writable: true,
+  value: jest.fn().mockReturnValue(true),
+})
+
